81ce4449f78c2ed71dd30937cfcf49aa4cff2580
Thai Quang Tung <tqtung@etri.re.kr>
2025-06-12 16:36:25 +0900

    re-generate

--- models/SmPolicyDecision.go
+++ models/SmPolicyDecision.go
@@ -8,16 +8,16 @@ package models
 
 type SmPolicyDecision struct {
 	Online                *bool                           `json:"online,omitempty"`
-	PccRules              map[string]PccRule              `json:"pccRules,omitempty"`
-	ChgDecs               map[string]ChargingData         `json:"chgDecs,omitempty"`
-	QosChars              map[string]QosCharacteristics   `json:"qosChars,omitempty"`
+	PccRules              map[string]*PccRule             `json:"pccRules,omitempty"`
+	ChgDecs               map[string]*ChargingData        `json:"chgDecs,omitempty"`
+	QosChars              map[string]*QosCharacteristics  `json:"qosChars,omitempty"`
 	LastReqUsageData      *RequestedUsageData             `json:"lastReqUsageData,omitempty"`
 	Ipv4Index             *int                            `json:"ipv4Index,omitempty"`
 	TsnPortManContDstt    *PortManagementContainer        `json:"tsnPortManContDstt,omitempty"`
 	TsnPortManContNwtts   []PortManagementContainer       `json:"tsnPortManContNwtts,omitempty"`
-	TraffContDecs         map[string]TrafficControlData   `json:"traffContDecs,omitempty"`
-	QosMonDecs            map[string]QosMonitoringData    `json:"qosMonDecs,omitempty"`
-	Conds                 map[string]ConditionData        `json:"conds,omitempty"`
+	TraffContDecs         map[string]*TrafficControlData  `json:"traffContDecs,omitempty"`
+	QosMonDecs            map[string]*QosMonitoringData   `json:"qosMonDecs,omitempty"`
+	Conds                 map[string]*ConditionData       `json:"conds,omitempty"`
 	SuppFeat              string                          `json:"suppFeat,omitempty"`
 	RedSessIndication     *bool                           `json:"redSessIndication,omitempty"`
 	ReflectiveQoSTimer    *int                            `json:"reflectiveQoSTimer,omitempty"`
@@ -26,13 +26,13 @@ type SmPolicyDecision struct {
 	QosDecs               map[string]QosData              `json:"qosDecs,omitempty"`
 	RevalidationTime      string                          `json:"revalidationTime,omitempty"`
 	PcscfRestIndication   *bool                           `json:"pcscfRestIndication,omitempty"`
-	UmDecs                map[string]UsageMonitoringData  `json:"umDecs,omitempty"`
+	UmDecs                map[string]*UsageMonitoringData `json:"umDecs,omitempty"`
 	Ipv6Index             *int                            `json:"ipv6Index,omitempty"`
 	TsnBridgeManCont      *BridgeManagementContainer      `json:"tsnBridgeManCont,omitempty"`
-	PraInfos              map[string]PresenceInfoRm       `json:"praInfos,omitempty"`
+	PraInfos              map[string]*PresenceInfoRm      `json:"praInfos,omitempty"`
 	QosFlowUsage          QosFlowUsage                    `json:"qosFlowUsage,omitempty"`
 	RelCause              SmPolicyAssociationReleaseCause `json:"relCause,omitempty"`
-	SessRules             map[string]SessionRule          `json:"sessRules,omitempty"`
+	SessRules             map[string]*SessionRule         `json:"sessRules,omitempty"`
 	Offline               *bool                           `json:"offline,omitempty"`
 	LastReqRuleData       []RequestedRuleData             `json:"lastReqRuleData,omitempty"`
 	ChargingInfo          *ChargingInformation            `json:"chargingInfo,omitempty"`
