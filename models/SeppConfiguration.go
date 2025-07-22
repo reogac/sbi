dca8addffd15170b4df05994d9df426d88eb73b4
Thai Quang Tung <tqtung@etri.re.kr>
2025-07-22 15:15:44 +0900

    add sepp configuration

---
+++ models/SeppConfiguration.go
@@ -0,0 +1,11 @@
+/*
+This file is generated with a SBI APIs generator tool developed by ETRI
+Generated at Tue Jul 22 15:13:42 KST 2025 by TungTQ<tqtung@etri.re.kr>
+Do not modify
+*/
+
+package models
+
+type SeppConfiguration struct {
+	Sepps []HomePlmnConfiguration `json:"sepps"`
+}
